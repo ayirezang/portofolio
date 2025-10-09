2ab309617b44cc4ca3c6c8ab388e6cc3c63fe578
ayirezang <patienceayirezang33@gmail.com>
2025-10-09 15:18:39 +0000

    firstPortofolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-5xl font-bold underline flex justify-center items-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
